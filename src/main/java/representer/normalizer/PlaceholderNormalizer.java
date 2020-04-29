e29fec01abfe93c062c382cbc7a2ee276c2b30fc
mirkoperillo <mirko.perillo@gmail.com>
2020-04-29 07:45:07 +0200

    fix #4

--- src/main/java/representer/normalizer/PlaceholderNormalizer.java
+++ src/main/java/representer/normalizer/PlaceholderNormalizer.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
@@ -36,6 +37,13 @@ public class PlaceholderNormalizer extends VoidVisitorAdapter<String> {
 
     @Override
     public void visit(NameExpr n, String arg) {
+        final String name = n.getNameAsString();
+        n.setName(mapper.getPlaceholder(name));
+        super.visit(n, arg);
+    }
+
+    @Override
+    public void visit(VariableDeclarator n, String arg) {
         final String name = n.getNameAsString();
         n.setName(mapper.getPlaceholder(name));
         super.visit(n, arg);
