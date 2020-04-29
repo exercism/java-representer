e839da0fbe5e356e80f4dc67e6792a5408b7deff
mirkoperillo <mirko.perillo@gmail.com>
2020-04-29 07:47:24 +0200

    substitute constructor name with placeholder

--- src/main/java/representer/normalizer/PlaceholderNormalizer.java
+++ src/main/java/representer/normalizer/PlaceholderNormalizer.java
@@ -3,6 +3,7 @@ package representer.normalizer;
 import java.util.Map;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.VariableDeclarator;
@@ -44,6 +45,13 @@ public class PlaceholderNormalizer extends VoidVisitorAdapter<String> {
 
     @Override
     public void visit(VariableDeclarator n, String arg) {
+        final String name = n.getNameAsString();
+        n.setName(mapper.getPlaceholder(name));
+        super.visit(n, arg);
+    }
+
+    @Override
+    public void visit(ConstructorDeclaration n, String arg) {
         final String name = n.getNameAsString();
         n.setName(mapper.getPlaceholder(name));
         super.visit(n, arg);
