13ccb687cdf3a9ca1c2eba873f2b0230a8159927
mirkoperillo <mirko.perillo@gmail.com>
2020-12-14 14:06:00 +0100

    placeholder for enumerations

--- src/main/java/representer/normalizer/PlaceholderNormalizer.java
+++ src/main/java/representer/normalizer/PlaceholderNormalizer.java
@@ -1,69 +1,203 @@
 package representer.normalizer;
 
 import java.util.Map;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.EnumConstantDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
 
 public class PlaceholderNormalizer extends VoidVisitorAdapter<String> {
-    private Mapper mapper = new Mapper();
+	private static final Logger logger = LogManager.getLogger(PlaceholderNormalizer.class);
 
-    @Override
-    public void visit(ClassOrInterfaceDeclaration n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	private Mapper mapper = new Mapper();
 
+	@Override
+	public void visit(ClassOrInterfaceDeclaration n, String arg) {
+		logger.debug("ClassOrInterfaceDeclaration: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
 
-    @Override
-    public void visit(MethodDeclaration n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	@Override
+	public void visit(MethodDeclaration n, String arg) {
+		logger.debug("MethodDeclaration: {}", n.getName().asString());
+		logger.debug(" --- " + n.getTypeAsString());
+		mapType(n.getType());
+		n.getParameters().forEach(p -> System.out.println(p.getType().asString()));
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
+	
+	private void mapType(Type t) {
+		if(t.isClassOrInterfaceType()) {
+			logger.debug("sono un tipo classe o interfaccia");
+		}
+		if(t.isArrayType()) {
+			logger.debug("sono un tipo array");
+			mapType(t.getElementType());
+		}
+		if(t.isPrimitiveType()) {
+			logger.debug("sono primitivo");
+		}
+		
+		if(t.isTypeParameter()) {
+			logger.debug("son un tipo parametro");
+		}
+		if(t.isVoidType()) {
+			logger.debug("sono void");
+		}
+	}
 
+	@Override
+	public void visit(Parameter n, String arg) {
+		logger.debug("Parameter: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
 
-    @Override
-    public void visit(Parameter n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	@Override
+	public void visit(NameExpr n, String arg) {
+		logger.debug("Name Expr: {}", n);
+		logger.debug("Name Expr: {}", n.getClass());
+		if (isUserDefined(qualifiedName(n))) {
+			final String name = n.getNameAsString();
+			n.setName(mapper.getPlaceholder(name));
+		}
+		super.visit(n, arg);
+	}
 
-    @Override
-    public void visit(NameExpr n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	@Override
+	public void visit(FieldAccessExpr n, String arg) {
+		logger.debug("FieldAccessExpr: {}", n);
+		Expression scope = n.getScope();
+		if (scope.isNameExpr()) {
+			if (isUserDefined(qualifiedName(scope.asNameExpr()))) {
+				logger.debug("user defined");
+				final String name = n.getNameAsString();
+				n.setName(mapper.getPlaceholder(name));
+			} else {
+				logger.debug("Java language");
+			}
+		}
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
 
-    @Override
-    public void visit(FieldAccessExpr n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
-    public void visit(VariableDeclarator n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	@Override
+	public void visit(ReturnStmt n, String arg) {
+		logger.debug("ReturnStmt: {}", n);
+		Optional<Expression> scope = n.getExpression();
+		scope.ifPresent(s -> {
+			if (s.isNameExpr()) {
+				if (isUserDefined(qualifiedName(s.asNameExpr()))) {
+					logger.debug("user defined");
+					final String name = s.asNameExpr().getName().asString();
+					s.asNameExpr().setName(mapper.getPlaceholder(name));
+				} else {
+					logger.debug("Java language");
+				}
+			}
+		});
+		super.visit(n, arg);
+	}
 
-    @Override
-    public void visit(ConstructorDeclaration n, String arg) {
-        final String name = n.getNameAsString();
-        n.setName(mapper.getPlaceholder(name));
-        super.visit(n, arg);
-    }
+	@Override
+	public void visit(MethodCallExpr n, String arg) {
+		logger.debug("MethodCallExpr: {}", n);
+		Optional<Expression> scope = n.getScope();
+		scope.ifPresent(s -> {
+			if (s.isNameExpr()) {
+				if (isUserDefined(qualifiedName(s.asNameExpr()))) {
+					logger.debug("user defined");
+					final String name = s.asNameExpr().getName().asString();
+					s.asNameExpr().setName(mapper.getPlaceholder(name));
+				} else {
+					logger.debug("Java language");
+				}
+			}
+		});
 
-    public Map<String, String> mapping() {
-        return mapper.getMapping();
-    }
+		if (!scope.isPresent()) {
+			final String name = n.getNameAsString();
+			n.setName(mapper.getPlaceholder(name));
+			n.getArguments().forEach(a -> {
+				String nn = a.asNameExpr().getName().asString();
+				a.asNameExpr().setName(mapper.getPlaceholder(nn));
+			});
+		}
+		super.visit(n, arg);
+	}
+
+	public void visit(VariableDeclarator n, String arg) {
+		logger.debug("VariableDeclarator: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
+
+	@Override
+	public void visit(ConstructorDeclaration n, String arg) {
+		logger.debug("ConstructorDeclaration: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
+
+	@Override
+	public void visit(EnumDeclaration n, String arg) {
+		logger.debug("EnumDeclaration: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
+
+	@Override
+	public void visit(EnumConstantDeclaration n, String arg) {
+		logger.debug("EnumConstantDeclaration: {}", n.getName().asString());
+		final String name = n.getNameAsString();
+		n.setName(mapper.getPlaceholder(name));
+		super.visit(n, arg);
+	}
+
+	public Map<String, String> mapping() {
+		return mapper.getMapping();
+	}
+
+	private boolean isUserDefined(String qualifiedName) {
+		return qualifiedName == null || !qualifiedName.startsWith("java.") && !qualifiedName.startsWith("javax.");
+	}
+
+	private String qualifiedName(NameExpr nameExpr) {
+		try {
+			logger.debug("--> " + nameExpr.getNameAsString());
+			if (Character.isUpperCase(nameExpr.getNameAsString().charAt(0))) {
+				return nameExpr.calculateResolvedType().describe();
+			} else {
+				return null;
+			}
+		} catch (UnsolvedSymbolException e) {
+			return null;
+		}
+	}
+
 }
