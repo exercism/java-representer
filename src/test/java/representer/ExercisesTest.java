d616045ca237983378ba477348601bc91eaa41a6
mirkoperillo <mirko.perillo@gmail.com>
2020-12-16 09:43:33 +0100

    clean code

--- src/test/java/representer/ExercisesTest.java
+++ src/test/java/representer/ExercisesTest.java
@@ -66,11 +66,8 @@ public class ExercisesTest {
     @Test
     public void allergiesRepresentation() throws Exception {
     	String testFolder = "exercises/allergies";
-    	String sourceContent = testUtils.getResourceContent(testFolder + "/Allergies.java");
     	List<String> sources = testUtils.getResourceContentFromFolder(testFolder);
-    	
     	String representation = sources.stream().map(s -> representer.generate(s, fakeRepresentationSerializator, fakeMappingSerializator)).collect(Collectors.joining());
-    	//String representation = representer.generate(sourceContent, fakeRepresentationSerializator, fakeMappingSerializator);
     	String expectedRepresentation = testUtils.getResourceContent(testFolder + "/representation");
     	System.out.println(representation);
     	Assertions.assertThat(representation).isEqualTo(expectedRepresentation);
